8b3b1ce988f314f6eccd721552ab0557dcdeb946
Bocatest <124800431+Bocatest@users.noreply.github.com>
2023-03-07 20:22:16 +0200

    Delete src directory

--- src/dash/dash.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Dash = () => {
-  return (
-    <div>dASH</div>
-  )
-}
-
-export default Dash;
--- src/home/home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home = () => {
-  return (
-    <div>hOME</div>
-  )
-}
-
-export default Home;
