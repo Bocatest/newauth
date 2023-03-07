200b1848a7440d3a16325ffd6852bdec0f81daa1
Bocatest <124800431+Bocatest@users.noreply.github.com>
2023-03-07 14:16:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<SignUp/>} />
+        <Route path="/" element={<SignIn/>} />
          <Route path="/signup" element={<SignUp/>}/>
         <Route path="/signin" element={<SignIn/>}/>
         <Route path="/profile" element={<ProfileSetup/>}/>
